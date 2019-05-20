a397afb4c56aecee9d384599dab2b32c19c47b7e
王强 <ActStrady@tom.com>
2019-05-20 09:29:38 +0800

    Initial commit

---
+++ douban/spiders/movie_scrapy.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# @Time : 2019/5/20 9:28
+# @Author : ActStrady@tom.com
+# @FileName : movie_scrapy.py
+# @GitHub : https://github.com/ActStrady/douban
